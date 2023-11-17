405bd8ae35d2168a4efc313c2f181fcbced88d92
Týna Wolo <tyna@Tynas-MacBook-Air.local>
2023-11-17 11:02:56 +0100

    Added first reactrouter structure, added header to App.jsx page.

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,31 @@
 import { createRoot } from 'react-dom/client';
+import React from 'react';
+import { App } from './App';
 import { HomePage } from './pages/HomePage';
 import './global.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-createRoot(
-  document.querySelector('#app'),
-).render(<HomePage />);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    // errorElement:<ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <HomePage />,
+      },
+      // {
+      //   path: '/MenuPage',
+      //   element: <MenuPage />,
+      // },
+      // {
+      //   path: '/gamepage',
+      //   element: <GamePage />,
+      // },
+    ],
+  },
+]);
+createRoot(document.querySelector('#app')).render(
+  <RouterProvider router={router} />,
+);
