12fe0f0d30517b2c55929178c1185f9f4271e309
MCHCortana <wolokika@gmail.com>
2023-11-21 18:52:07 +0100

    Removed grid From game_menu_page replaced by flexbox for better responsivity

--- src/pages/MenuPage/index.jsx
+++ src/pages/MenuPage/index.jsx
@@ -6,40 +6,41 @@ export const MenuPage = () => {
   return (
     <main className="container__menu_page">
       <h1 className="userName">Ahoj Martínku!</h1>
-      <img className="dino-mino__menupage" src="./img/DinoMinoNoShadow.png" />
       <img
         className="faq-menupage"
         src="./img/IconsFunctional/faq.png"
         alt="FAQ"
       />
-      <div className="main_menu">
-        <div className="menu_icon--one-two">
-          <div>
-            <Link to="/activitypage">
+      <div className="menu_page-content">
+        <img className="dino-mino__menupage" src="./img/DinoMinoNoShadow.png" />
+        <div className="main_menu">
+          <div className="menu_icon--one-two">
+            <div>
+              <Link to="/activitypage">
+                <img
+                  className="menu_page--calendar"
+                  src="./img/IconsFunctional/calendar.png"
+                  alt="Kalendář"
+                />
+              </Link>
+            </div>
+            <div>
               <img
-                className="menu_page--calendar"
-                src="./img/IconsFunctional/calendar.png"
-                alt="Kalendář"
+                className="menu_page--planner"
+                src="./img/IconsFunctional/planner.png"
+                alt="Plánovač"
+              />
+            </div>
+          </div>
+          <div>
+            <Link to="/gamepage">
+              <img
+                className="menu_page--puzzle"
+                src="./img/IconsFunctional/puzzle.png"
+                alt="Hry"
               />
             </Link>
           </div>
-          <div>
-            <img
-              className="menu_page--planner"
-              src="./img/IconsFunctional/planner.png"
-              alt="Plánovač"
-            />
-          </div>
-        </div>
-
-        <div>
-          <Link to="/gamepage">
-            <img
-              className="menu_page--puzzle"
-              src="./img/IconsFunctional/puzzle.png"
-              alt="Hry"
-            />
-          </Link>
         </div>
       </div>
     </main>
