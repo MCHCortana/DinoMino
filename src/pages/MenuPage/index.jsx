26b939df4520c264a87c81c5e35ef4e4d96b4485
MCHCortana <wolokika@gmail.com>
2023-11-21 08:04:40 +0100

    Added some animation on hover fro icons

--- src/pages/MenuPage/index.jsx
+++ src/pages/MenuPage/index.jsx
@@ -1,9 +1,12 @@
-import './style.css';
+// MenuPage.jsx
+
+import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
+import './style.css';
 
 export const MenuPage = () => {
   return (
@@ -11,52 +14,59 @@ export const MenuPage = () => {
       <Container className="grid-menu_page">
         <Row>
           <Col className="userName">
-            {' '}
             <h1>Ahoj Martínku!</h1>
           </Col>
-          <Row>
-            <div className="main_menu">
-              <Row className="row1-menu_page">
-                <Col>
-                  <Link to="/activitypage">
-                    <img
-                      className="menu_page--calendar"
-                      src="./img/IconsFunctional/calendar.png"
-                    />
-                  </Link>
-                </Col>
-                <Col></Col>
-                <Col></Col>
-                <Col>
+        </Row>
+        <Row>
+          <div className="main_menu">
+            <Row className="row1-menu_page">
+              <Col>
+                <Link to="/activitypage">
                   <img
-                    className="menu_page--planner"
-                    src="./img/IconsFunctional/planner.png"
+                    className="menu_page--calendar"
+                    src="./img/IconsFunctional/calendar.png"
+                    alt="Calendar"
                   />
-                </Col>
-              </Row>
-              <Row className="row2-menu_page">
-                <Col>
-                  <Link to="/gamepage">
-                    <img
-                      className="menu_page--puzzle"
-                      src="./img/IconsFunctional/puzzle.png"
-                    />
-                  </Link>
-                </Col>
-              </Row>
-            </div>
-          </Row>
+                </Link>
+              </Col>
+              <Col></Col>
+              <Col></Col>
+              <Col>
+                <img
+                  className="menu_page--planner"
+                  src="./img/IconsFunctional/planner.png"
+                  alt="Planner"
+                />
+              </Col>
+            </Row>
+            <Row className="row2-menu_page">
+              <Col>
+                <Link to="/gamepage">
+                  <img
+                    className="menu_page--puzzle"
+                    src="./img/IconsFunctional/puzzle.png"
+                    alt="Puzzle"
+                  />
+                </Link>
+              </Col>
+            </Row>
+          </div>
         </Row>
         <Row>
           <Col>
             <img
               className="dino-mino__menupage"
               src="./img/DinoMinoNoShadow.png"
+              alt="Dino Mino"
             />
           </Col>
           <Col xs lg="4"></Col>
           <Col>
-            <img className="faq-menupage" src="./img/IconsFunctional/faq.png" />
+            <img
+              className="faq-menupage"
+              src="./img/IconsFunctional/faq.png"
+              alt="FAQ"
+            />
           </Col>
         </Row>
       </Container>
