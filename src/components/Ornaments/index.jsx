69e21bd35ec5e7a0ebe6ddef8c9d8fad3fd3053e
MCHCortana <wolokika@gmail.com>
2023-11-22 23:26:32 +0100

    Added christmas tree basic structure

--- src/components/Ornaments/index.jsx
+++ src/components/Ornaments/index.jsx
@@ -1,2 +1,18 @@
 import './style.css';
-export const Ornaments = () => {};
+import { useState } from 'react';
+export const Ornaments = ({ fill, number }) => {
+  let fillShape = 'empty';
+  if (fill === 's') {
+    fillShape = 'filled star';
+  } else if (fill === 'x') {
+    fillShape = 'filled filledB';
+  } else if (fill === 'g') {
+    fillShape = 'filled filledG';
+  }
+
+  return (
+    <div className={fillShape}>
+      <h3>{number}</h3>
+    </div>
+  );
+};
