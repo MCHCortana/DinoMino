55dd399d89449676e9c13e894f27a47b05adff55
Tetiana Vrba <tetiana.vrba@gmail.com>
2023-11-23 10:56:48 +0100

    Component Mounths added without functionality

---
+++ src/pages/ActivityPage/SummaryActivityPage/Mounths/index.jsx
@@ -0,0 +1,85 @@
+import './style.css';
+
+export const Mounths = () => {
+  return (
+    <main class="box">
+      <div class="mounths-box">
+        <div class="season">
+          <div class="mounths">
+            <div class="mounth-box january">
+              <p>Leden</p>
+            </div>
+            <div class="mounth-box february">
+              <p>Únor</p>
+            </div>
+          </div>
+          <div class="winter-img">
+            <img class="season-winter" src="img/3 (1).png" alt="Winter" />
+          </div>
+        </div>
+
+        <div class="season">
+          <div class="spring-img">
+            <img class="season-spring" src="img/4 (1).png" alt="Spring" />
+          </div>
+          <div class="mounths">
+            <div class="mounth-box march">
+              <p>Březen</p>
+            </div>
+            <div class="mounth-box april">
+              <p>Duben</p>
+            </div>
+            <div class="mounth-box may">
+              <p>Květen</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="season">
+          <div class="mounths">
+            <div class="mounth-box june">
+              <p>Červen</p>
+            </div>
+            <div class="mounth-box july">
+              <p>Červenec</p>
+            </div>
+            <div class="mounth-box august">
+              <p>Srpen</p>
+            </div>
+          </div>
+          <div class="summer-img">
+            <img class="season-summer" src="img/5.png" alt="Summer" />
+          </div>
+        </div>
+
+        <div class="season">
+          <div class="autumn-img">
+            <img class="season-autumn" src="img/2 (1).png" alt="Autumn" />
+          </div>
+          <div class="mounths">
+            <div class="mounth-box september">
+              <p>Září</p>
+            </div>
+            <div class="mounth-box october">
+              <p>Říjen</p>
+            </div>
+            <div class="mounth-box november">
+              <p>Listopad</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="season">
+          <div class="mounths">
+            <div class="mounth-box december">
+              <p>Prosinec</p>
+            </div>
+          </div>
+          <div class="winter-img">
+            <img class="season-winter" src="img/3 (1).png" alt="Winter" />
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
