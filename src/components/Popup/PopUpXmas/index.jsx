80c98e0c7f9e6b4e864298f45c67a7f52d49ede9
MCHCortana <wolokika@gmail.com>
2023-11-24 09:23:06 +0100

    Functional Xmas popup, Xmas page styling not optimal (meaning its awful), will be fixed later

--- src/components/Popup/PopUpXmas/index.jsx
+++ src/components/Popup/PopUpXmas/index.jsx
@@ -1,9 +1,7 @@
 import './style.css';
-import { useState } from 'react';
 
 export const PopUpXmas = ({ popContent }) => {
   const { id, element, number, text, name } = popContent;
-
   return (
     <div className="popup_xmas">
       <h2>
@@ -19,14 +17,23 @@ export const PopUpXmas = ({ popContent }) => {
       {number % 7 === 0 && number / 7 === 1 && (
         <h2>Ježíšek přijde už za jeden týden.</h2>
       )}
-      {24 - number === 0 && <h2>Dnes je ŠTĚDRÝ DEN</h2>}
+      {24 - number === 0 && (
+        <>
+          <h2>Dnes je ŠTĚDRÝ DEN</h2>
+          <img src="./img/IconsCalendar/music-notes.png" alt="Noty" />
+        </>
+      )}
+      <h1>Říkanka dne:</h1>
       <h2>{text}</h2>
       <div>
         <h2>🎁Omalovánka k vytisknutí.🎁</h2>
-        <img
-          className="coloring"
-          src={`./img/coloring/omalovanky${number.toString()}.jpg`}
-        />
+        <a href={`./img/coloring/omalovanky${number.toString()}.jpg`}>
+          <img
+            className="coloring"
+            src={`./img/coloring/omalovanky${number.toString()}.jpg`}
+            alt="Omalovánka ke stažení"
+          />
+        </a>
       </div>
     </div>
   );
