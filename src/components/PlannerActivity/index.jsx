d9c1eca92a7e737c2d4363f16ce7df5841aae6f2
MCHCortana <wolokika@gmail.com>
2023-11-27 21:27:57 +0100

    comment on Planner activity condition

--- src/components/PlannerActivity/index.jsx
+++ src/components/PlannerActivity/index.jsx
@@ -4,7 +4,7 @@ export const PlannerActivity = ({activity, delete}) => {
   return (
 
       <div>
-{delete && <button><img src="./img/remove.png " /> </button>}
+{/* {delete && <button><img src="./img/remove.png " /> </button>} */}
         
         <img src={activity.src} alt={activity.alt} />
       </div>
