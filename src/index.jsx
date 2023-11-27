c93e9007426e9e436591d90fe710ce8bcaa8d3e2
Tetiana Vrba <tetiana.vrba@gmail.com>
2023-11-27 18:37:44 +0100

    Monster basic structure, handlePrint function

--- src/index.jsx
+++ src/index.jsx
@@ -38,7 +38,7 @@ const router = createBrowserRouter([
         element: <Xmas />,
       },
       {
-        path: '/monster',
+        path: '/Monster',
         element: <Monster />,
       },
     ],
